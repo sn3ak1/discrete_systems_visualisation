747b9b28356cf000fa45c76e21a32fd2ed654b72
sn3aky <konradlab01@gmail.com>
2022-11-23 18:06:49 +0100

    new firebase

--- src/App.js
+++ src/App.js
@@ -23,13 +23,13 @@ var trilat = require("trilat");
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBrzfiHgu_LZGq9qniVyxOp5rf7ZqQsOgA",
-  authDomain: "discretesystems-fbef2.firebaseapp.com",
-  projectId: "discretesystems-fbef2",
-  storageBucket: "discretesystems-fbef2.appspot.com",
-  messagingSenderId: "355100286047",
-  appId: "1:355100286047:web:e7a06fd14996ad7d730fea",
-  measurementId: "G-EJJVYYDEY5",
+  apiKey: "AIzaSyDvMtOoYR1rdVxi4Fdmjvm7x64opzVeFAs",
+  authDomain: "ds02-6afa7.firebaseapp.com",
+  projectId: "ds02-6afa7",
+  storageBucket: "ds02-6afa7.appspot.com",
+  messagingSenderId: "906552624391",
+  appId: "1:906552624391:web:459c710bb8d169a6db31a2",
+  measurementId: "G-MXR6RLJJTQ",
 };
 
 // Initialize Firebase
@@ -71,24 +71,10 @@ async function readData() {
   points2.pop();
   // }
 
-  // console.log(points2);
-
-  let points_sorted = points2.map((point) => {
-    return point.sort(function (a, b) {
-      return a.x - b.x || a.y - b.y;
-    });
-  });
-
-  // console.log(points_sorted);
-
-  let result = points_sorted.map((point) => {
+  let result = points2.map((point) => {
     // console.log(trilat(meanPoint(point)));
     return trilat(meanPoint(point));
   });
-
-  // console.log(result);
-
-  // return result;
 
   return result.filter(
     (doc) => doc[0] > 0 && doc[1] > 0 && doc[0] < 10 && doc[1] < 10
