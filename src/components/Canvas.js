dd0ad98fa692da8a0f37bb512d6f86305e5186fa
sn3aky <konradlab01@gmail.com>
2023-01-12 02:34:01 +0100

    slider canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,57 +1,49 @@
 import React, { useEffect, useRef } from "react";
+import Slider from '@mui/material/Slider';
 
 export function Canvas({ data }) {
   const canvas = useRef();
-  let ctx = null;
+
+  const [value, setValue] = React.useState();
 
   // initialize the canvas context
   useEffect(() => {
-    // dynamically assign the width and height to canvas
     const canvasEle = canvas.current;
-    // canvasEle.width = 4.36 * 1000;
-    // canvasEle.height = 3.77 * 1000;
-
-    // get context of the canvas
-    ctx = canvasEle.getContext("2d");
+    canvas.current.width = 1000;
+    canvas.current.height = 1000;
   }, []);
 
-  let draw = (x1, y1, x2, y2) => {
+  let drawLine = (x1, y1, x2, y2) => {
+    const ctx = canvas.current.getContext("2d");
+
     ctx.beginPath();
     ctx.moveTo(x1 * 100, y1 * 100);
     ctx.lineTo(x2 * 100, y2 * 100);
     ctx.strokeStyle = "#000";
-    ctx.lineWidth = 5;
+    ctx.lineWidth = 10;
     ctx.stroke();
   };
 
   useEffect(() => {
-    ctx.beginPath();
-    ctx.arc(5, 5, 5, 0, 2 * Math.PI);
-    ctx.fillStyle = "#F00";
-    ctx.fill();
+    canvas.current.getContext("2d").clearRect(0, 0, canvas.current.width, canvas.current.height);
 
-    ctx.beginPath();
-    ctx.arc(5, 3.77 * 100, 5, 0, 2 * Math.PI);
-    ctx.fillStyle = "#F00";
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.arc(4.36 * 100, 3.77 * 100, 5, 0, 2 * Math.PI);
-    ctx.fillStyle = "#F00";
-    ctx.fill();
-
-    for (let i = 1; i < data.length; i++) {
-      draw(data[i - 1][0], data[i - 1][1], data[i][0], data[i][1]);
+    for (let i = 1; i < Math.round(value * data.length / 100); i++) {
+      drawLine(data[i - 1][0], data[i - 1][1], data[i][0], data[i][1]);
     }
+  }, [value]);
 
-    // data.forEach((point) => {
-    //   draw(point.x, point.y);
-    // });
-  }, []);
 
   return (
-    <div>
-      <canvas ref={canvas}></canvas>
+    <div style={{
+      display: 'flex', flexDirection: 'column', justifyContent: 'center',
+      alignItems: 'center', width: '80%', height: '70vh', margin: 'auto'
+    }}>
+      <Slider
+        onChange={(_, value) => {
+          setValue(value);
+        }}>
+      </Slider>
+      <canvas ref={canvas} style={{ height: '100%', border: '1px solid black' }}></canvas>
     </div>
   );
 }
