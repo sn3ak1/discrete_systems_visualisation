a38b1104ea51e78120c6a9391c8319c79499c959
Jan Karpiuk <karpiukjan@student.agh.edu.pl>
2023-01-13 00:09:14 +0100

    Display gps data from database, use time threshold for multiple lines

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import logo from "./logo.svg";
+import logo from "./graphics/icons/logo.svg";
 
 import React, { useState, useEffect, useRef } from "react";
 
 import "./App.css";
 
-import MapWrapper from "./MapWrapper";
 import { NavBar } from "./components/NavBar";
 
 // openlayers
-import GeoJSON from 'ol/format/GeoJSON'
 import {
   fetchBleDevicesNames, getBleDevicesNames, fetchBleData, fetchGpsDevicesNames,
   fetchGpsData, isDataFetched, getGpsDevicesNames, getBleData, getGpsData
 } from "./services/dataService";
 import Ble from "./components/Ble";
+import Gps from "./components/Gps";
+
 
 
 function App() {
@@ -29,7 +29,6 @@ function App() {
 
 
   // set intial state
-  const [features, setFeatures] = useState([]);
   const [mapView, setMapView] = useState(true);
 
 
@@ -60,57 +59,29 @@ function App() {
     }
   }, [refreshData])
 
-
-
-  // initialization - retrieve GeoJSON features from Mock JSON API get features from mock 
-  //  GeoJson API (read from flat .json file in public directory)
-  useEffect(() => {
-    const fetchData = async () => { 
-      await fetch('./mock-geojson-api.json')
-      .then(response => response.json() )
-      .then((fetchedFeatures) => {
-
-        // parse fetched geojson into OpenLayers features
-        //  use options to convert feature from EPSG:4326 to EPSG:3857
-        const wktOptions = {
-          dataProjection: 'EPSG:4326',
-          featureProjection: 'EPSG:3857'
-        }
-        const parsedFeatures = new GeoJSON().readFeatures(fetchedFeatures, wktOptions)
-
-        // set features into state (which will be passed into OpenLayers
-        //  map component as props)
-        setFeatures(parsedFeatures)
-        console.log("Fetched features!", parsedFeatures)
-      });
-    }
-    fetchData();
-  }, [])
-
   if (!flag) {
     return <div>Loading...</div>;
   }
 
-  console.log("Features passed to map: ", features);
   return (
     <div className="App">
       <NavBar
         changeViewHandler={() => setMapView(!mapView)}
         refreshDataHandler={() => setRefreshData(true)}
         deviceChangeHandler={(e) => setSelectedDevice(e.target.value)}
-        devices={bleDevices.current} />
+        devices={gpsDevices.current} />
 
       {!mapView &&
         <Ble
           data={bleData.current[selectedDevice]}>
-        </Ble>}
+        </Ble>
+      }
 
       {mapView &&
-        <div style={{ height: '90vh' }}>
-          <MapWrapper
-            features={features}>
-          </MapWrapper>
-        </div>}
+        <Gps
+          data={gpsData.current[selectedDevice]}>
+        </Gps>
+      }
     </div>
   );
 }
