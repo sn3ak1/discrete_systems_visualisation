f07d71e962a7bec8ca560564dac31cd6bd530b67
sn3aky <konradlab01@gmail.com>
2023-01-12 20:07:48 +0100

    canvas updating colored

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,25 +1,49 @@
 import React, { useEffect, useRef } from "react";
 import Slider from '@mui/material/Slider';
+const Rainbow = require('rainbowvis.js');
+const rainbow = new Rainbow();
+
+const SCALAR = 100;
 
 export function Canvas({ data }) {
   const canvas = useRef();
+  const slider = useRef();
 
-  const [value, setValue] = React.useState();
+  const [value, setValue] = React.useState(0);
+  const [sliderTouched, setSliderTouched] = React.useState(false);
 
-  // initialize the canvas context
   useEffect(() => {
-    const canvasEle = canvas.current;
     canvas.current.width = 1000;
     canvas.current.height = 1000;
+
+    rainbow.setNumberRange(0, data.length);
+    rainbow.setSpectrum('red', 'green');
   }, []);
 
-  let drawLine = (x1, y1, x2, y2) => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((value) => {
+        if (sliderTouched || value === data.length) {
+          clearInterval(interval);
+          return value;
+        }
+        return value + 1;
+      });
+    }, 100);
+    return () => clearInterval(interval);
+  }, [sliderTouched]);
+
+  let drawLine = (x1, y1, x2, y2, lineIndex = 1) => {
     const ctx = canvas.current.getContext("2d");
 
+    var grad = ctx.createLinearGradient(x1 * SCALAR, y1 * SCALAR, x2 * SCALAR, y2 * SCALAR);
+    grad.addColorStop(0, '#' + rainbow.colorAt(lineIndex - 1));
+    grad.addColorStop(1, '#' + rainbow.colorAt(lineIndex));
+
     ctx.beginPath();
-    ctx.moveTo(x1 * 100, y1 * 100);
-    ctx.lineTo(x2 * 100, y2 * 100);
-    ctx.strokeStyle = "#000";
+    ctx.moveTo(x1 * SCALAR, y1 * SCALAR);
+    ctx.lineTo(x2 * SCALAR, y2 * SCALAR);
+    ctx.strokeStyle = grad;
     ctx.lineWidth = 10;
     ctx.stroke();
   };
@@ -27,8 +51,8 @@ export function Canvas({ data }) {
   useEffect(() => {
     canvas.current.getContext("2d").clearRect(0, 0, canvas.current.width, canvas.current.height);
 
-    for (let i = 1; i < Math.round(value * data.length / 100); i++) {
-      drawLine(data[i - 1][0], data[i - 1][1], data[i][0], data[i][1]);
+    for (let i = 1; i < value; i++) {
+      drawLine(data[i - 1][0], data[i - 1][1], data[i][0], data[i][1], i);
     }
   }, [value]);
 
@@ -36,14 +60,22 @@ export function Canvas({ data }) {
   return (
     <div style={{
       display: 'flex', flexDirection: 'column', justifyContent: 'center',
-      alignItems: 'center', width: '80%', height: '70vh', margin: 'auto'
+      alignItems: 'center', width: '80%', height: '80vh', margin: 'auto', padding: '10px'
     }}>
       <Slider
-        onChange={(_, value) => {
+        ref={slider}
+        max={data.length}
+        value={value}
+        onChange={(event, value) => {
+          console.log(event);
+          if (event?.type === 'mousedown' || event?.type === 'touchstart') {
+            setSliderTouched(true);
+          }
           setValue(value);
-        }}>
+        }}
+      >
       </Slider>
-      <canvas ref={canvas} style={{ height: '100%', border: '1px solid black' }}></canvas>
+      <canvas ref={canvas} style={{ height: '100%', border: '1px solid black', marginTop: '15px' }}></canvas>
     </div>
   );
 }
